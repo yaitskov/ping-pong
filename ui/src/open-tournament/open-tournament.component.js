e7fc01dbf75a56307861ba922119d609913ab773
Daneel Yaitskov <dyaitskov@gmail.com>
2017-07-11 15:59:57 +0200

    master refresh every minute open tournament page

--- ui/src/open-tournament/open-tournament.component.js
+++ ui/src/open-tournament/open-tournament.component.js
@@ -14,34 +14,39 @@ angular.
                          self.tournament = null;
                          self.tid = $routeParams.tournamentId;
                          var params = {tournamentId: $routeParams.tournamentId};
-                         $q.all([
-                             Match.listOpenForWatch(params).$promise,
-                             Match.winners(params).$promise,
-                             Tournament.aMine(params).$promise
-                         ]).then(
-                             function (responses) {
-                                 self.matches = responses[0];
-                                 self.winners = response[1];
-                                 self.tournament = response[2];
-                                 mainMenu.setTitle('Matches of ' + self.tournament.name);
-                             },
-                             function (error) {
-                                 if (error.status == 502) {
-                                     self.error = "Server is not available";
-                                 } else if (error.status == 400) {
-                                     self.error = "Tournament is complete";
-                                 } else if (error.status == 500) {
-                                     if (typeof error.data == 'string') {
-                                         self.error = "Server error" + (error.data.indexOf('<') < 0 ? '' : ' ' + error.data);
-                                     } else if (typeof error.data == 'object') {
-                                         self.error = "Server error: " + error.data;
+                         self.load = function () {
+                             $q.all([
+                                 Match.listOpenForWatch(params).$promise,
+                                 Match.winners(params).$promise,
+                                 Tournament.aMine(params).$promise
+                             ]).then(
+                                 function (responses) {
+                                     self.error = null;
+                                     self.matches = responses[0];
+                                     self.winners = responses[1];
+                                     self.tournament = responses[2];
+                                     mainMenu.setTitle('Matches of ' + self.tournament.name);
+                                     $timeout(self.load, 60 * 1000);
+                                 },
+                                 function (error) {
+                                     if (error.status == 502) {
+                                         self.error = "Server is not available";
+                                     } else if (error.status == 400) {
+                                         self.error = "Tournament is complete";
+                                     } else if (error.status == 500) {
+                                         if (typeof error.data == 'string') {
+                                             self.error = "Server error" + (error.data.indexOf('<') < 0 ? '' : ' ' + error.data);
+                                         } else if (typeof error.data == 'object') {
+                                             self.error = "Server error: " + error.data;
+                                         } else {
+                                             self.error = "Server error";
+                                         }
                                      } else {
-                                         self.error = "Server error";
+                                         self.error = "Failed to load tournaments";
                                      }
-                                 } else {
-                                     self.error = "Failed to load tournaments";
-                                 }
-                             });
+                                 });
+                         };
+                         self.load();
                      }
                     ]
         });

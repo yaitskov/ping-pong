07a7799da7fd59eb0ad57a1f17ff5ec7a6f2e2bc
Daneel Yaitskov <dyaitskov@gmail.com>
2017-07-12 10:06:22 +0200

    master open tab after one time token sign in

--- ui/src/dev/emails-with-sign-in-token-list/emails-with-sign-in-token-list.component.js
+++ ui/src/dev/emails-with-sign-in-token-list/emails-with-sign-in-token-list.component.js
@@ -8,6 +8,17 @@ angular.module('emailsWithSignInTokenList').
                          this.error = null;
                          this.emails = null;
                          var self = this;
+                         this.signIn = function (email, token) {
+                             $http.get('/api/anonymous/auth/by-one-time-token/' + token + '/' + email).then(
+                                 function (resp) {
+                                     localStorage.putItem('mySession', resp.data.session);
+                                     localStorage.putItem('myUid', resp.data.session);
+                                     localStorage.putItem('myEmail', email);
+                                 },
+                                 function (bad) {
+                                     self.error = "Failed " + bad.status + " " + bad.data;
+                                 });
+                         };
                          $http.get('/api/dev/emails-with-sign-in-token', {}).
                              then(
                                  function (ok) {

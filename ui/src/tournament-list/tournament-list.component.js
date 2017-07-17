09b12a14b16639adc2434059b392ec9085e71344
Daneel Yaitskov <dyaitskov@gmail.com>
2017-07-17 12:16:06 +0200

    master mobile style for tournament list

--- ui/src/tournament-list/tournament-list.component.js
+++ ui/src/tournament-list/tournament-list.component.js
@@ -3,10 +3,25 @@
 angular.module('tournamentList').
     component('tournamentList', {
         templateUrl: 'tournament-list/tournament-list.template.html',
-        controller: ['Tournament', 'mainMenu',
-                     function (Tournament, mainMenu) {
+        controller: ['Tournament', 'mainMenu', '$location',
+                     function (Tournament, mainMenu, $location) {
                          mainMenu.setTitle('Drafting');
-                         this.tournaments = Tournament.drafting();
+                         this.tournaments = null;
+                         var self = this;
+                         self.error = null;
+                         this.goTo = function (tid) {
+                             $location.path('/tournaments/' + tid);
+                         };
+                         Tournament.drafting(
+                             {},
+                             function (tournaments) {
+                                 self.error = 0;
+                                 self.tournaments = tournaments;
+                             },
+                             function (bad) {
+                                 self.error = "Failed " + bad.status;
+                             });
+
                      }
                     ]
         });

0a5af1f7b312f9b2d00b299d545a93ead217484c
Daneel Yaitskov <dyaitskov@gmail.com>
2017-07-17 12:47:30 +0200

    master enable sass

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -3,11 +3,25 @@
 // npm install grunt-dependency-installer --save-dev
 // npm install grunt-install-dependencies --save-dev
 // npm install grunt-html-build --save-dev
+// npm install grunt-contrib-sass --save-dev
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-install-dependencies');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json')
     });
-    grunt.registerTask('all', ['install-dependencies']);
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'node_modules/app/app.css': 'src/app.scss'
+                }
+            }
+        }
+    });
+    grunt.registerTask('all', ['install-dependencies', 'sass']);
     grunt.registerTask('default', ['all']);
 };

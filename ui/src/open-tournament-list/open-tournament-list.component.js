a9a4e7b137498aaabf5fa6b05ec7fd3b24aea0e8
Daneel Yaitskov <dyaitskov@gmail.com>
2017-07-18 17:59:31 +0200

    master use request-status in open tournament list

--- ui/src/open-tournament-list/open-tournament-list.component.js
+++ ui/src/open-tournament-list/open-tournament-list.component.js
@@ -3,32 +3,19 @@
 angular.module('openTournamentList').
     component('openTournamentList', {
         templateUrl: 'open-tournament-list/open-tournament-list.template.html',
-        controller: ['Tournament', 'mainMenu',
-                     function (Tournament, mainMenu) {
+        controller: ['Tournament', 'mainMenu', 'requestStatus',
+                     function (Tournament, mainMenu, requestStatus) {
                          mainMenu.setTitle('Running tournaments');
                          var self = this;
                          self.tournaments = null;
+                         requestStatus.startLoading();
                          Tournament.running(
                              {},
                              function (tournaments) {
                                  self.tournaments = tournaments;
+                                 requestStatus.complete();
                              },
-                             function (error) {
-                                 self.tournaments = [];
-                                 if (error.status == 502) {
-                                     self.error = "Server is not available";
-                                 } else if (error.status == 500) {
-                                     if (typeof error.data == 'string') {
-                                         self.error = "Server error" + (error.data.indexOf('<') < 0 ? '' : ' ' + error.data);
-                                     } else if (typeof error.data == 'object') {
-                                         self.error = "Server error: " + self.error.message;
-                                     } else {
-                                         self.error = "Server error";
-                                     }
-                                 } else {
-                                     self.error = "Failed to load tournaments";
-                                 }
-                             });
+                             requestStatus.failed);
                      }
                     ]
         });

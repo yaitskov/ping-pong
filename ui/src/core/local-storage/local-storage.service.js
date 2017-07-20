5b3a5d7772776e6b1ea835dda1bb4005562dd8c3
Daneel Yaitskov <dyaitskov@gmail.com>
2017-07-20 16:52:22 +0200

    master balls

--- ui/src/core/local-storage/local-storage.service.js
+++ ui/src/core/local-storage/local-storage.service.js
@@ -5,16 +5,14 @@ angular.
     factory('LocalStorage', [function () {
         return new function () {
             this.store = function(key, value) {
-                //console.log("Set [" + key + "] = [" + value + "]");
                 localStorage.setItem(key, value)
             };
             this.get = function (key) {
                 var value = localStorage.getItem(key)
-                //console.log("Get [" + key + "] = [" + value + "]");
                 return value;
             };
             this.clearAll = function () {
-                while (localStorage.length >= 0) {
+                while (localStorage.length > 0) {
                     localStorage.removeItem(localStorage.key(0));
                 }
             };

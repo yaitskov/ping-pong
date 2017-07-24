80cc149d8e95de4c316fd4f0b4b4e58812a99ebb
Daneel Yaitskov <dyaitskov@gmail.com>
2017-07-24 15:15:32 +0200

    master detect tournament end for admin

--- ui/src/my-match-judge-list/my-match-judge-list.component.js
+++ ui/src/my-match-judge-list/my-match-judge-list.component.js
@@ -3,43 +3,38 @@
 angular.module('myMatchJudgeList').
     component('myMatchJudgeList', {
         templateUrl: 'my-match-judge-list/my-match-judge-list.template.html',
-        controller: ['Match', 'Tournament', 'mainMenu', '$q', '$location', 'pageCtx',
-                     function (Match, Tournament, mainMenu, $q, $location, pageCtx) {
+        controller: ['Match', 'Tournament', 'mainMenu', '$q', '$location', 'pageCtx', 'requestStatus',
+                     function (Match, Tournament, mainMenu, $q, $location, pageCtx, requestStatus) {
                          mainMenu.setTitle('Match Judgement');
                          this.matches = null;
                          this.tournament = null;
+                         var self = this;
+                         this.previouslyScoredEnded = null;
+                         this.checkTournamentEnd = function () {
+                             self.previouslyScored = (
+                                 pageCtx.get('last-admin-scoring-match-id')
+                                     && pageCtx.get('last-admin-scoring-match-id') == self.tournament.previous.tid);
+                         };
                          this.completeMatch = function (mid, idx) {
+                             pageCtx.put('last-admin-scoring-match-id', this.matches[idx].tid);
                              pageCtx.putMatchParticipants(mid, this.matches[idx].participants);
                              $location.path('/complete/match/' + mid);
                          };
-                         var self = this;
-                         self.error = null;
+                         self.iSawTournamentEnd = function () {
+                             pageCtx.put('last-admin-scoring-match-id', null);
+                             self.previouslyScored = null;
+                         };
+                         requestStatus.startLoading();
                          $q.all([Tournament.myRecentJudgements({}).$promise,
                                  Match.myMatchesNeedToJudge({}).$promise]).
                              then(
                                  function (responses) {
+                                     requestStatus.complete();
                                      self.tournament = responses[0];
                                      self.matches = responses[1];
-                                     self.error = null;
+                                     self.checkTournamentEnd();
                                  },
-                                 function (error) {
-                                     self.matches = [];
-                                     if (error.status == 502) {
-                                         self.error = "Server is not available";
-                                     } else if (error.status == 401) {
-                                         self.error = "Session is not valid. Click link to send an email with authentication link.";
-                                     } else if (error.status == 500) {
-                                         if (typeof error.data == 'string') {
-                                             self.error = "Server error" + (error.data.indexOf('<') < 0 ? '' : ' ' + error.data);
-                                         } else if (typeof error.data == 'object') {
-                                             self.error = "Server error: " + self.error.message;
-                                         } else {
-                                             self.error = "Server error";
-                                         }
-                                     } else {
-                                         self.error = "Failed to load matches";
-                                     }
-                                 });
+                                 requestStatus.failed);
                      }
                     ]
         });
--- ui/src/my-match-judge-list/my-match-judge-list.module.js
+++ ui/src/my-match-judge-list/my-match-judge-list.module.js
@@ -1,4 +1,5 @@
 'use strict';
 
 angular.module('myMatchJudgeList', ['ngRoute', 'core.match',
+                                    'core.requestStatus',
                                     'core.tournament', 'mainMenu']);

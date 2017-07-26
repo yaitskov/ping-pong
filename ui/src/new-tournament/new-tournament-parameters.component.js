a65d9d304b9d809079d28fdee0e5e0915500c9ef
Daneel Yaitskov <dyaitskov@gmail.com>
2017-07-26 12:07:31 +0200

    master touranment params

--- ui/src/new-tournament/new-tournament-parameters.component.js
+++ ui/src/new-tournament/new-tournament-parameters.component.js
@@ -10,11 +10,21 @@ angular.
                          this.tournament = pageCtx.get('newTournament') || {quitsFromGroup: 2,
                                                                             maxGroupSize: 8,
                                                                             matchScore: 3};
+                         this.options = {
+                             quitsFromGroup: {min: 1, max: 5},
+                             maxGroupSize: {min: 2, max: 20},
+                             matchScore: {min: 1, max: 100}
+                         };
                          this.published = this.tournament.tid;
                          var self = this;
                          this.createTournament = function () {
                              self.form.$setSubmitted();
                              if (!self.form.$valid) {
+                                 return;
+                             }
+                             if (self.tournament.maxGroupSize <= self.tournament.quitsFromGroup) {
+                                 requestStatus.validationFailed(
+                                     'Max group size is less than number of people quitting group');
                                  return;
                              }
                              requestStatus.startLoading('Publishing');

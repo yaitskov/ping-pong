72b4b91f57c1902ee9ba20fe33714901b28dfcc9
Daneel Yaitskov <dyaitskov@gmail.com>
2017-08-02 15:51:07 +0200

    master simulate almost complete

--- ping-pong/src/test/java/org/dan/ping/pong/simulate/TwoCategoriesSim.java
+++ ping-pong/src/test/java/org/dan/ping/pong/simulate/TwoCategoriesSim.java
@@ -1,11 +1,21 @@
 package org.dan.ping.pong.simulate;
 
+import static org.dan.ping.pong.mock.simulator.Pause.BeforeScore;
 import static org.dan.ping.pong.mock.simulator.Player.p1;
 import static org.dan.ping.pong.mock.simulator.Player.p2;
 import static org.dan.ping.pong.mock.simulator.Player.p3;
 import static org.dan.ping.pong.mock.simulator.Player.p4;
+import static org.dan.ping.pong.mock.simulator.Player.p5;
+import static org.dan.ping.pong.mock.simulator.Player.p6;
+import static org.dan.ping.pong.mock.simulator.Player.p7;
+import static org.dan.ping.pong.mock.simulator.Player.p8;
+import static org.dan.ping.pong.mock.simulator.Player.p9;
+import static org.dan.ping.pong.mock.simulator.Player.pa;
 import static org.dan.ping.pong.mock.simulator.PlayerCategory.c1;
 import static org.dan.ping.pong.mock.simulator.PlayerCategory.c2;
+import static org.dan.ping.pong.mock.simulator.PlayerCategory.c3;
+import static org.dan.ping.pong.mock.simulator.PlayerCategory.c4;
+import static org.dan.ping.pong.mock.simulator.SimulatorParams.T_1_Q_1_G_2;
 import static org.dan.ping.pong.mock.simulator.SimulatorParams.T_1_Q_1_G_8;
 
 import lombok.SneakyThrows;
@@ -39,5 +49,21 @@ public class TwoCategoriesSim {
                         .doNotBegin()
                         .presence(EnlistMode.Enlist, p4)
                         .presence(EnlistMode.Pass, p1, p2, p3));
+    }
+
+    @Test
+    public void almostComplete() {
+        simulator.simulate(T_1_Q_1_G_2, TournamentScenario.begin()
+                .name("almostComplete")
+                .ignoreUnexpectedGames()
+                .category(c1, p1, p2)
+                .category(c2, p3, p4, p5, p6, p7, p8, p9, pa)
+                .w30(p3, p4)
+                .w30(p5, p6)
+                .w30(p7, p8)
+                .w30(p9, pa)
+                .quitsGroup(p3, p5, p7, p9)
+                .w31(p3, p5)
+                .w31(p7, p9));
     }
 }

58c0c0697d220abc208c8b8efbbe3f7d09269c2e
Daneel Yaitskov <dyaitskov@gmail.com>
2017-08-21 15:35:51 +0200

    master main menu translation

--- ui/src/tournament-list/tournament-list.component.js
+++ ui/src/tournament-list/tournament-list.component.js
@@ -4,12 +4,9 @@ import template from './tournament-list.template.html';
 angular.module('tournamentList').
     component('tournamentList', {
         templateUrl: template,
-        controller: ['Tournament', 'mainMenu', '$location', 'requestStatus', '$translate',
-                     function (Tournament, mainMenu, $location, requestStatus, $translate) {
-                         $translate('Drafting').then(
-                             function (drafting) {
-                                 mainMenu.setTitle(drafting);
-                             });
+        controller: ['Tournament', 'mainMenu', '$location', 'requestStatus',
+                     function (Tournament, mainMenu, $location, requestStatus) {
+                         mainMenu.setTitle('Drafting');
                          this.tournaments = null;
                          var self = this;
                          requestStatus.startLoading();
--- ui/src/translate.js
+++ ui/src/translate.js
@@ -1,10 +1,32 @@
 module.exports = {
     pl: {
         'no-drafting-tournament-found': 'Nie znależiono żadnego turnieja otwartego do zapisu.',
-        'Drafting': 'Rekrutacja'
+        'Drafting': 'Rekrutacja',
+
+        // main menu
+        'Watch Games': 'Oglądaj gry',
+        'My Schedule': 'Moje mecze',
+        'Participation': 'Uczestniczę w',
+        'Administration': 'Administracja',
+        'My Judgement': 'Sądownictwo',
+        'My Places': 'Moi boiska',
+        'Login': 'Zaloguj się',
+        'Sign-Up': 'Zapisz się',
+        'Account': 'Konto'
     },
     en: {
         'no-drafting-tournament-found': 'There is no any dratfing tournament.',
-        'Drafting': 'Drafting'
+        'Drafting': 'Drafting',
+
+        // main menu
+        'Watch Games': 'Watch Games',
+        'My Schedule': 'My Schedule',
+        'Participation': 'Participation',
+        'Administration': 'Administration',
+        'My Judgement': 'My Judgement',
+        'My Places': 'My Places',
+        'Login': 'Login',
+        'Sign-Up': 'Sign-Up',
+        'Account': 'Account'
     }
 };

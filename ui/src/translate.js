ce16234f5a690c128544b39a257f8810df452e20
Daneel Yaitskov <dyaitskov@gmail.com>
2017-08-22 10:29:05 +0200

    master translate account page

--- ui/src/translate.js
+++ ui/src/translate.js
@@ -20,7 +20,28 @@ module.exports = {
         'Server is not available': 'Serwer jest nie dostępny',
         'session-expired': 'Sesja wygasła. Wyloguj i zaloguj się ponownie jeśli twoje konto ma przywiązany email',
         'no-permissions': 'Nie masz odpowiednich uprawnień',
-        'application-error': 'Wystąpił błąd aplikacji'
+        'application-error': 'Wystąpił błąd aplikacji',
+
+        // account page
+        'Logout': 'Wyloguj',
+        'Generate sign-in link': 'Wyslać link do logowania',
+        'Edit': 'Edytować',
+        'Request Admin permissions': 'Chcę sądzić',
+        'sing-in-lin-generated': 'Link do logowania był wysłany na maila.',
+        'account-without-email-warning': 'Twoje konto nie ma skrzynki email. '
+            + 'Straczisz dostęp do konta na zawsze w razie wylogowania. '
+            + 'Bez skrzynki też nie możesz zalogować się z innych urządzeń.',
+        'Terminate account': 'Usunąć konto',
+        'Set an email': 'Ustawić adres',
+        'Email': 'Email',
+        'email-is-not-set': 'Email nie ustawiony. Email jest potrzebny do logowania z innych urządzeń'
+            + ' lub do regeneracji sesji.',
+        'follow-set-email-link': 'Zastosuj <a href="{{url}}">linka</a>, żeby ustawić email',
+        'first and last name': 'Imię i nazwisko',
+        'Phone': 'Number telefonu',
+        'Not set': 'Nie ustawione',
+        'User type': 'Typ użytkownika',
+        'Admin': 'Administrator'
     },
     en: {
         'no-drafting-tournament-found': 'There is no any dratfing tournament.',
@@ -43,6 +64,16 @@ module.exports = {
         'Server is not available': 'Server is not available',
         'session-expired': 'Session is not valid. Try to logout and login again if your account is bound to an email.',
         'no-permissions': 'Not enough permissions',
-        'application-error': 'An application error happened'
+        'application-error': 'An application error happened',
+
+        // account page
+        'sing-in-lin-generated': 'Sign-in link has been generated and mailed to you.',
+        'account-without-email-warning': 'Your account doesn\'t have an email bound to it. If you logout '
+            + 'right now, you will invalidate account forever! To logout from '
+            + 'this device and keep account - setup an email first.',
+        'email-is-not-set': 'An email is not set. It\'s required to login from other devices'
+            + ' or to recover current session.',
+        'follow-set-email-link': 'Follow <a href="{{url}}">the link</a> to set an email',
+        'first and last name': 'Name',
     }
 };

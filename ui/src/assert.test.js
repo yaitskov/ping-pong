4d86088e307216185e33189bf16228c57993b7da
Daneel Yaitskov <dyaitskov@gmail.com>
2017-08-28 09:33:51 +0200

    master configure mocha test

---
+++ ui/src/assert.test.js
@@ -0,0 +1,11 @@
+// import chai from 'chai';
+const assert = require('assert');
+
+
+describe('Demo', () => {
+  it('should add correctly', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+//assert.equal(1,1);

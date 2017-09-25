1371da91176789c39e0720b279a3cb1344dc1a74
Daneel Yaitskov <dyaitskov@gmail.com>
2017-09-25 18:36:27 +0200

    master drag and drop sorting participants on touch including

--- ui/src/ping-pong-main.js
+++ ui/src/ping-pong-main.js
@@ -1,5 +1,6 @@
 import 'flatpickr/dist/flatpickr.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery-ui-dist/jquery-ui.css';
 import 'clockpicker/dist/bootstrap-clockpicker.css';
 import 'bootstrap-toggle/css/bootstrap-toggle.css';
 import 'angular-touchspin/dist/angular-touchspin.css';
@@ -7,6 +8,8 @@ import 'angular-touchspin/dist/angular-touchspin.css';
 import './css/app.scss';
 
 import jQuery from 'jquery';
+import 'jquery-ui-dist/jquery-ui.js';
+import 'jquery-ui-touch-punch/jquery.ui.touch-punch.js';
 import moment from 'moment/moment.js';
 import flatpickr from 'flatpickr';
 import angular from 'angular';
@@ -14,6 +17,7 @@ import angularRoute from 'angular-route';
 import angularResource from 'angular-resource';
 import angularMoment from 'angular-moment/angular-moment.js';
 import angularTouchSpin from 'angular-touchspin';
+import uiSortable from 'angular-ui-sortable/src/sortable.js';
 import bootstrap from 'bootstrap/dist/js/bootstrap.js';
 import bootstrapClockPicker from 'clockpicker/dist/bootstrap-clockpicker.js';
 import 'bootstrap-toggle/js/bootstrap-toggle.js';
--- ui/src/tournament/participant/participant.module.js
+++ ui/src/tournament/participant/participant.module.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 
 angular.module('participant', [
-    'ngRoute', 'core.tournament', 'mainMenu', 'auth', 'core',
-    'core.requestStatus', 'core.participant', 'core.tournamentStatus'
+    'ngRoute', 'ui.sortable',
+    'core.tournament', 'mainMenu', 'auth', 'core',
+    'core.requestStatus', 'core.participant',
+    'core.tournamentStatus'
 ]);

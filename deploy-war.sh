a8fe1103a57bec34f43a5327aa5feaf24d2b4af0
Daneel Yaitskov <dyaitskov@gmail.com>
2017-10-08 14:16:09 +0200

    master delpoy-war

---
+++ deploy-war.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WARDIR=bin/jetty-distribution-9.3.9.v20160517/base/webapps
+
+scp ping-pong/target/ping-pong-1.0.0-SNAPSHOT.war \
+        diaitskov@pp:~/$WARDIR/../ROOT-next-$$.war || exit 1
+
+ssh diaitskov@pp ./deploy-war.sh $$

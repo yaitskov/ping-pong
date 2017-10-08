d9479f8fe68d9a0acda20c5b1c70a205566fe1d4
Daneel Yaitskov <dyaitskov@gmail.com>
2017-10-08 14:54:43 +0200

    master fix angular loop

--- ui/src/complete-match/review-match-score.component.js
+++ ui/src/complete-match/review-match-score.component.js
@@ -12,17 +12,11 @@ angular.
                          self.conflict =
                          self.matchScore = pageCtx.get('match-score-review-' + $routeParams.matchId);
                          self.participants = pageCtx.getMatchParticipants($routeParams.matchId);
-
-                         this.sets = function () {
-                             var result = [];
-                             var l = self.matchScore.sets[self.participants[0].uid].length;
-                             for (var i = 0; i < l; ++i) {
-                                 result.push({a: self.matchScore.sets[self.participants[0].uid][i],
-                                              b: self.matchScore.sets[self.participants[1].uid][i]});
-                             }
-                             return result;
-                         };
-                         this.isWon = function (set) {
-                             return set.a > set.b;
-                         };
+                         var result = [];
+                         var l = self.matchScore.sets[self.participants[0].uid].length;
+                         for (var i = 0; i < l; ++i) {
+                             result.push({a: self.matchScore.sets[self.participants[0].uid][i],
+                             b: self.matchScore.sets[self.participants[1].uid][i]});
+                         }
+                         this.sets = result;
                      }]});

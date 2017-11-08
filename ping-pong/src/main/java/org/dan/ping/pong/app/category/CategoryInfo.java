b73c0f256ce2a1b69484c62deb56561075f87b78
Daneel Yaitskov <dyaitskov@gmail.com>
2017-11-08 13:06:39 +0100

    master link to category info from match management

---
+++ ping-pong/src/main/java/org/dan/ping/pong/app/category/CategoryInfo.java
@@ -0,0 +1,21 @@
+package org.dan.ping.pong.app.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.dan.ping.pong.app.user.UserLink;
+import org.dan.ping.pong.app.user.UserRole;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLinkList {
+    private UserRole role;
+    private List<UserLink> users;
+}

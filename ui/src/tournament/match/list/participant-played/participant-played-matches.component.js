c76933fbc177484507e36234525f7f6f0da1d5b9
Daneel Yaitskov <dyaitskov@gmail.com>
2017-11-08 12:20:23 +0100

    master dedup

--- ui/src/tournament/match/list/participant-played/participant-played-matches.component.js
+++ ui/src/tournament/match/list/participant-played/participant-played-matches.component.js
@@ -5,11 +5,20 @@ angular.
     module('tournament').
     component('participantPlayedMatches', {
         templateUrl: template,
-        controller: ['Match', 'mainMenu', '$routeParams', 'requestStatus', 'binder', '$scope',
-                     function (Match, mainMenu, $routeParams, requestStatus, binder, $scope) {
+        controller: ['Match', 'mainMenu', '$routeParams', 'requestStatus', 'binder', '$scope', 'eBarier', '$rootScope',
+                     function (Match, mainMenu, $routeParams, requestStatus, binder, $scope, eBarier, $rootScope) {
                          var self = this;
                          self.tournamentId = $routeParams.tournamentId;
+                         var inGroupBarier = eBarier.create(['widget', 'data'], (list) => {
+                             $rootScope.$broadcast('event.complete.match.list.data.inGroup', list);
+                         });
+                         var playOffBarier = eBarier.create(['widget', 'data'], (list) => {
+                             $rootScope.$broadcast('event.complete.match.list.data.playOff', list);
+                         });
+
                          binder($scope, {
+                             'event.complete.match.list.ready.inGroup': (e) => inGroupBarier.got('widget'),
+                             'event.complete.match.list.ready.playOff': (e) => playOffBarier.got('widget'),
                              'event.main.menu.ready': (e) => mainMenu.setTitle('Matches played by me'),
                              'event.request.status.ready': (event) => {
                                  requestStatus.startLoading();
@@ -18,6 +27,8 @@ angular.
                                      function (matches) {
                                          requestStatus.complete();
                                          self.matches = matches;
+                                         inGroupBarier.got('data', matches.inGroup);
+                                         playOffBarier.got('data', matches.playOff);
                                      },
                                      requestStatus.failed);
                              }

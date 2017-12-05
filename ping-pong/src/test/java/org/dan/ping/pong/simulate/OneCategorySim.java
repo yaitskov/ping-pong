cd20e30e4be6899757c27d4b89e6a655b34dc605
Daneel Yaitskov <dyaitskov@gmail.com>
2017-12-05 14:38:44 +0100

    master twoGroupsBy2AlmostComplete

--- ping-pong/src/test/java/org/dan/ping/pong/simulate/OneCategorySim.java
+++ ping-pong/src/test/java/org/dan/ping/pong/simulate/OneCategorySim.java
@@ -1,6 +1,7 @@
 package org.dan.ping.pong.simulate;
 
 import static org.dan.ping.pong.app.match.MatchJerseyTest.RULES_G2Q1_S1A2G11;
+import static org.dan.ping.pong.app.match.MatchJerseyTest.RULES_G3Q2_S1A2G11;
 import static org.dan.ping.pong.app.match.MatchJerseyTest.RULES_G8Q1_S1A2G11;
 import static org.dan.ping.pong.app.match.MatchJerseyTest.RULES_G8Q1_S3A2G11;
 import static org.dan.ping.pong.app.match.MatchJerseyTest.RULES_G8Q2_S1A2G11;
@@ -14,12 +15,13 @@ import static org.dan.ping.pong.mock.simulator.Player.p4;
 import static org.dan.ping.pong.mock.simulator.Player.p5;
 import static org.dan.ping.pong.mock.simulator.Player.p6;
 import static org.dan.ping.pong.mock.simulator.PlayerCategory.c1;
+import static org.dan.ping.pong.mock.simulator.TournamentScenario.begin;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.dan.ping.pong.mock.simulator.EnlistMode;
 import org.dan.ping.pong.mock.simulator.Simulator;
-import org.dan.ping.pong.mock.simulator.TournamentScenario;
+import org.dan.ping.pong.mock.simulator.imerative.ImperativeSimulatorFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -29,7 +31,7 @@ import javax.inject.Inject;
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {SimulationCtx.class})
+@ContextConfiguration(classes = {ImperativeSimulatorFactory.class, SimulationCtx.class})
 public class OneCategorySim {
     @Inject
     private Simulator simulator;
@@ -38,7 +40,7 @@ public class OneCategorySim {
     @SneakyThrows
     public void pauseBeforeSecondScore() {
         simulator.simulate(
-                TournamentScenario.begin()
+                begin()
                         .rules(RULES_G8Q1_S3A2G11)
                         .ignoreUnexpectedGames()
                         .autoResolution(RANDOM)
@@ -53,7 +55,7 @@ public class OneCategorySim {
     @SneakyThrows
     public void justEnlist() {
         simulator.simulate(
-                TournamentScenario.begin()
+                begin()
                         .rules(RULES_G8Q1_S3A2G11)
                         .ignoreUnexpectedGames()
                         .name("justEnlist")
@@ -67,7 +69,7 @@ public class OneCategorySim {
     @SneakyThrows
     public void schedule3ConcurrentGames() {
         simulator.simulate(
-                TournamentScenario.begin()
+                begin()
                         .tables(3)
                         .rules(RULES_G8Q1_S3A2G11)
                         .ignoreUnexpectedGames()
@@ -79,7 +81,7 @@ public class OneCategorySim {
     @SneakyThrows
     public void justBeginTournamentOf2() {
         simulator.simulate(
-                TournamentScenario.begin()
+                begin()
                         .rules(RULES_G8Q1_S3A2G11)
                         .ignoreUnexpectedGames()
                         .name("justBeginTournamentOf2")
@@ -90,7 +92,7 @@ public class OneCategorySim {
     @SneakyThrows
     public void justBeginTournamentOf2To1() {
         simulator.simulate(
-                TournamentScenario.begin()
+                begin()
                         .rules(RULES_G8Q1_S1A2G11)
                         .ignoreUnexpectedGames()
                         .name("justBeginTourOf2To1")
@@ -101,7 +103,7 @@ public class OneCategorySim {
     @SneakyThrows
     public void justBeginTournamentOf3() {
         simulator.simulate(
-                TournamentScenario.begin()
+                begin()
                         .rules(RULES_G8Q1_S3A2G11)
                         .ignoreUnexpectedGames()
                         .name("justBeginTournamentOf3")
@@ -110,7 +112,7 @@ public class OneCategorySim {
 
     @Test
     public void quits2Of3() {
-        simulator.simulate(TournamentScenario.begin()
+        simulator.simulate(begin()
                 .name("quits2Of3")
                 .rules(RULES_G8Q2_S1A2G11)
                 .category(c1, p1, p2, p3)
@@ -124,7 +126,7 @@ public class OneCategorySim {
 
     @Test(expected = AssertionError.class)
     public void quits2Of2() {
-        simulator.simulate(TournamentScenario.begin()
+        simulator.simulate(begin()
                 .ignoreUnexpectedGames()
                 .rules(RULES_G8Q2_S1A2G11)
                 .name("quits2Of2")
@@ -133,7 +135,7 @@ public class OneCategorySim {
 
     @Test
     public void tournamentOf2() {
-        simulator.simulate(TournamentScenario.begin()
+        simulator.simulate(begin()
                 .ignoreUnexpectedGames()
                 .rules(RULES_G8Q1_S1A2G11)
                 .name("tournamentOf2")
@@ -142,7 +144,7 @@ public class OneCategorySim {
 
     @Test
     public void almostComplete() {
-        simulator.simulate(TournamentScenario.begin()
+        simulator.simulate(begin()
                 .name("almostComplete")
                 .rules(RULES_G2Q1_S1A2G11)
                 .ignoreUnexpectedGames()
@@ -157,7 +159,7 @@ public class OneCategorySim {
 
     @Test
     public void scoreLastSet() {
-        simulator.simulate(TournamentScenario.begin()
+        simulator.simulate(begin()
                 .name("scoreLastSet")
                 .rules(RULES_G8Q1_S3A2G11)
                 .ignoreUnexpectedGames()
@@ -165,5 +167,27 @@ public class OneCategorySim {
                 .custom(game(p1, p2, 11, 0, 11, 1, 0, 0, 11, 2))
                 .quitsGroup(p1)
                 .champions(c1, p1));
+    }
+
+    @Inject
+    private ImperativeSimulatorFactory isf;
+
+    @Test
+    public void twoGroupsBy2AlmostComplete() {
+        isf.create(begin().name("2GroupsBy2AlmostCompleteS1")
+                .rules(RULES_G3Q2_S1A2G11)
+                .category(c1, p1, p2, p3, p4, p5, p6))
+                .run(c -> c.beginTournament()
+                        .scoreSet(p1, 11, p3, 3)
+                        .scoreSet(p4, 11, p6, 6)
+
+                        .scoreSet(p1, 11, p2, 2)
+                        .scoreSet(p4, 11, p5, 1)
+
+                        .scoreSet(p2, 11, p3, 5)
+                        .scoreSet(p5, 11, p6, 7)
+
+                        .scoreSet(p1, 11, p5, 5)
+                        .scoreSet(p4, 11, p2, 2));
     }
 }

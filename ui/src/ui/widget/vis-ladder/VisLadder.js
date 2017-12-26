91fd70c89ebada6ee1a7fceb8da8ea91f09b9bcc
Daneel Yaitskov <dyaitskov@gmail.com>
2017-12-26 18:45:25 +0100

    master extract VisLadder and recolor ladder

---
+++ ui/src/ui/widget/vis-ladder/VisLadder.js
@@ -0,0 +1,85 @@
+import vis from 'vis';
+import matchLabel from './match-label.js';
+import NodeMenuManager from 'ui/vis-popup-menu/NodeMenuManager.js';
+import MatchManagementLinkFactory from 'ui/vis-popup-menu/MatchManagementLinkFactory.js';
+
+export default class VisLadder {
+    createMatchNode(tournament, match) {
+        return {id: match.id, level: match.level,
+                label: matchLabel(tournament, match),
+                shape: 'box'};
+    };
+
+    get options() {
+        return {
+            autoResize: true,
+            height: '100%',
+            width: '100%',
+            manipulation: false,
+            nodes: {
+                font: {
+                    face: 'mono',
+                    size: 20,
+                    color: 'black'
+                },
+                color: {
+                    background: '#bbb',
+                    border: 'black'
+                },
+                margin: {
+                    right: 10
+                }
+            },
+            edges: {
+                arrows: 'to',
+                color: {
+                    color: 'green',
+                    inherit: false
+                }
+            },
+            layout: {
+                hierarchical: {
+                    direction: 'LR',
+                    enabled: true,
+                    levelSeparation: 400,
+                    sortMethod: "directed"
+                },
+                //randomSeed: 2
+            },
+            physics: {
+                hierarchicalRepulsion: {
+                    nodeDistance: 80,
+                }
+            }
+        };
+    }
+
+    data(tournament) {
+        let nodes = [];
+        for (let match of tournament.matches) {
+            nodes.push(this.createMatchNode(tournament, match));
+        }
+        let edges = [];
+        for (let edge of tournament.transitions) {
+            edge.arrow = 'to';
+            edges.push(edge);
+        }
+        return {
+            nodes: new vis.DataSet(nodes),
+            edges: new vis.DataSet(edges)
+        };
+    }
+
+    constructor(container, tournament) {
+        let options = this.options;
+        let data = this.data(tournament);
+        const network = new vis.Network(container, data, options);
+        let menuManager = new NodeMenuManager(
+            $(container),
+            network,
+            options,
+            data.nodes,
+            new MatchManagementLinkFactory(tournament.tid));
+        network.once('afterDrawing', () => network.fit());
+    }
+}
--- ui/src/ui/widget/vis-ladder/vis-ladder.component.js
+++ ui/src/ui/widget/vis-ladder/vis-ladder.component.js
@@ -1,10 +1,7 @@
 import angular from 'angular';
-import vis from 'vis';
 import './vis-ladder.css';
-import matchLabel from './match-label.js';
 import template from './vis-ladder.template.html';
-import NodeMenuManager from 'ui/vis-popup-menu/NodeMenuManager.js';
-import MatchManagementLinkFactory from 'ui/vis-popup-menu/MatchManagementLinkFactory.js';
+import VisLadder from './VisLadder.js';
 
 angular.
     module('widget').
@@ -12,74 +9,9 @@ angular.
         templateUrl: template,
         controller: ['$scope', '$rootScope', 'binder', '$attrs', '$window', 'cutil',
                      function ($scope, $rootScope, binder, $attrs, $window, cutil) {
-                         var self = this;
-
-                         self.createMatchNode = function (tournament, match) {
-                             var node = {id: match.id, level: match.level,
-                                         label: matchLabel(tournament, match),
-                                         shape: 'box'};
-                             return node;
-                         };
-
-                         self.loadData = function (tournament) {
-                             var nodes = [];
-                             for (var i in tournament.matches) {
-                                 var match = tournament.matches[i];
-                                 nodes.push(self.createMatchNode(tournament, match));
-                             }
-                             var nodeSet = new vis.DataSet(nodes);
-
-                             var edges = [];
-                             for (var i in tournament.transitions) {
-                                 var edge = tournament.transitions[i];
-                                 edge.arrow = 'to';
-                                 edges.push(edge);
-                             }
-
-                             var edgeSet = new vis.DataSet(edges);
-                             var container = document.getElementById('play-off-chart');
-                             var data = {
-                                 nodes: nodeSet,
-                                 edges: edgeSet
-                             };
-                             var options = {
-                                 autoResize: true,
-                                 height: '100%',
-                                 width: '100%',
-                                 manipulation: false,
-                                 nodes: {
-                                     font: {
-                                         face: 'mono'
-                                     }
-                                 },
-                                 edges: {
-                                     arrows: 'to'
-                                 },
-                                 layout: {
-                                     hierarchical: {
-                                         direction: 'LR',
-                                         enabled: true,
-                                         levelSeparation: 400,
-                                         sortMethod: "directed"
-                                     },
-                                     //randomSeed: 2
-                                 },
-                                 physics: {
-                                     hierarchicalRepulsion: {
-                                         nodeDistance: 80,
-                                     }
-                                 }
-                             };
-
-                             let menuManager = new NodeMenuManager(
-                                 $(container),
-                                 new vis.Network(container, data, options),
-                                 options,
-                                 nodeSet,
-                                 new MatchManagementLinkFactory(tournament.tid));
-                         };
+                         let container = document.getElementById('play-off-chart');
                          binder($scope, {
-                             'event.playoff.view.data': (event, tournament) => self.loadData(tournament),
+                             'event.playoff.view.data': (e, tournament) => new VisLadder(container, tournament),
                          });
                          $rootScope.$broadcast('event.playoff.view.ready');
                      }]

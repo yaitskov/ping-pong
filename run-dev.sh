8cb6018d9dca188da3599f575cb69600cbd16948
Daneel Yaitskov <dyaitskov@gmail.com>
2017-12-29 16:42:44 +0100

    console run-dev.sh

---
+++ run-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=${PORT:-5001},server=y,suspend=n"
+
+mvn -pl ping-pong -P run-server -DskipTests install

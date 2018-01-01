545d78a7679b89c402433cd343a19a8f538c5b7a
Daneel Yaitskov <dyaitskov@gmail.com>
2018-01-01 12:21:46 +0100

    master fix ProvidedRating is always selected

--- ui/src/tournament/parameters/param.form.component.js
+++ ui/src/tournament/parameters/param.form.component.js
@@ -85,7 +85,6 @@ angular.
                              rules.playOff = rules.playOff || defaultPlayOffRules;
                              rules.casting.providedRankOptions = (rules.casting.providedRankOptions
                                                                   || defaultProvidedRankOptions);
-                             rules.casting.policy = 'ProvidedRating';
                              if (!rules.group.schedule || !rules.group.schedule.size2Schedule) {
                                  rules.group.schedule = defaultGroupRules.schedule;
                              }

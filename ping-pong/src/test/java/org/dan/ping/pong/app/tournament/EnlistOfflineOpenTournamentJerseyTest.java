a2bdc4db109584eebe7b3ee631080490f349850e
Daneel Yaitskov <dyaitskov@gmail.com>
2018-01-04 13:14:07 +0100

    master enlistToTournamentWith2GroupsAutoSelect

--- ping-pong/src/test/java/org/dan/ping/pong/app/tournament/EnlistOfflineOpenTournamentJerseyTest.java
+++ ping-pong/src/test/java/org/dan/ping/pong/app/tournament/EnlistOfflineOpenTournamentJerseyTest.java
@@ -3,8 +3,10 @@ package org.dan.ping.pong.app.tournament;
 import static org.dan.ping.pong.app.bid.BidState.Expl;
 import static org.dan.ping.pong.app.bid.BidState.Lost;
 import static org.dan.ping.pong.app.bid.BidState.Win1;
+import static org.dan.ping.pong.app.bid.BidState.Win2;
 import static org.dan.ping.pong.app.group.GroupResource.CID;
 import static org.dan.ping.pong.app.group.GroupResource.GROUP_POPULATIONS;
+import static org.dan.ping.pong.app.match.MatchJerseyTest.RULES_G2Q1_S1A2G11;
 import static org.dan.ping.pong.app.match.MatchJerseyTest.RULES_G8Q1_S1A2G11;
 import static org.dan.ping.pong.app.match.MatchResource.BID_PENDING_MATCHES;
 import static org.dan.ping.pong.app.tournament.TournamentResource.TOURNAMENT_ENLIST_OFFLINE;
@@ -13,6 +15,7 @@ import static org.dan.ping.pong.mock.simulator.Player.p1;
 import static org.dan.ping.pong.mock.simulator.Player.p2;
 import static org.dan.ping.pong.mock.simulator.Player.p3;
 import static org.dan.ping.pong.mock.simulator.Player.p4;
+import static org.dan.ping.pong.mock.simulator.Player.p5;
 import static org.dan.ping.pong.mock.simulator.PlayerCategory.c1;
 import static org.junit.Assert.assertThat;
 
@@ -26,6 +29,7 @@ import org.dan.ping.pong.mock.simulator.PlayHook;
 import org.dan.ping.pong.mock.simulator.Simulator;
 import org.dan.ping.pong.mock.simulator.TournamentScenario;
 import org.dan.ping.pong.mock.simulator.imerative.BidStatesDesc;
+import org.dan.ping.pong.mock.simulator.imerative.ImperativeSimulator;
 import org.dan.ping.pong.mock.simulator.imerative.ImperativeSimulatorFactory;
 import org.dan.ping.pong.test.AbstractSpringJerseyTest;
 import org.hamcrest.Matchers;
@@ -45,6 +49,48 @@ public class EnlistOfflineOpenTournamentJerseyTest extends AbstractSpringJerseyT
 
     @Inject
     private ImperativeSimulatorFactory isf;
+
+    @Test
+    public void enlistToGroupWithParticipantPlayedAllGames() {
+    }
+
+    @Test
+    public void enlistToTournamentWith2GroupsAutoSelect() {
+        final TournamentScenario scenario = TournamentScenario
+                .begin()
+                .tables(2)
+                .name("enlistToTournamentWith2Groups")
+                .rules(RULES_G2Q1_S1A2G11)
+                .category(c1, p1, p2, p3, p4);
+        final ImperativeSimulator simulator = isf.create(scenario);
+        simulator.run(c -> {
+            c.beginTournament();
+            final int cid = scenario.getCategoryDbId().get(c1);
+
+            final GroupPopulations populations = myRest()
+                    .get(GROUP_POPULATIONS + scenario.getTid().getTid() + CID + cid,
+                            GroupPopulations.class);
+
+            final Uid joinedUid = myRest().post(TOURNAMENT_ENLIST_OFFLINE, scenario,
+                    EnlistOffline.builder()
+                            .groupId(Optional.of(populations.getLinks().get(1).getGid()))
+                            .tid(scenario.getTid())
+                            .cid(cid)
+                            .bidState(BidState.Wait)
+                            .name("p5")
+                            .build()).readEntity(Uid.class);
+            scenario.addPlayer(joinedUid, p5);
+            c.scoreSet(p1, 11, p2, 2)
+                    .scoreSet(p3, 11, p4, 7)
+                    .scoreSet(p3, 11, p5, 8)
+                    .scoreSet(p4, 11, p5, 9)
+                    //
+                    .scoreSet(p1, 11, p3, 4)
+                    .checkResult(p1, p3, p4, p5, p2)
+                    .checkTournamentComplete(BidStatesDesc
+                            .restState(Lost).bid(p3, Win2).bid(p1, Win1));
+        });
+    }
 
     @Test
     public void enlistToGroupWithResignedParticipant() {

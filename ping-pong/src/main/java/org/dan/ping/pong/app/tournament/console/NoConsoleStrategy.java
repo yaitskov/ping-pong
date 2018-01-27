c30e102aafaf2d4a7005aa9e8ccfaf2efd6f5152
Daneel Yaitskov <dyaitskov@gmail.com>
2018-01-27 16:07:01 +0100

    console fix tests

--- ping-pong/src/main/java/org/dan/ping/pong/app/tournament/console/NoConsoleStrategy.java
+++ ping-pong/src/main/java/org/dan/ping/pong/app/tournament/console/NoConsoleStrategy.java
@@ -1,11 +1,15 @@
 package org.dan.ping.pong.app.tournament.console;
 
 import static java.util.Arrays.asList;
+import static org.dan.ping.pong.app.bid.BidState.Expl;
 import static org.dan.ping.pong.app.bid.BidState.Lost;
+import static org.dan.ping.pong.app.bid.BidState.Quit;
 import static org.dan.ping.pong.app.bid.BidState.Rest;
 import static org.dan.ping.pong.app.bid.BidState.Wait;
 
+import com.google.common.collect.ImmutableSet;
 import org.dan.ping.pong.app.bid.BidService;
+import org.dan.ping.pong.app.bid.BidState;
 import org.dan.ping.pong.app.bid.Uid;
 import org.dan.ping.pong.app.tournament.TournamentMemState;
 import org.dan.ping.pong.sys.db.DbUpdater;
@@ -18,10 +22,13 @@ public class NoConsoleStrategy implements ConsoleStrategy {
     @Inject
     private BidService bidService;
 
+    private static final Set<BidState> terminalStates = ImmutableSet.of(Quit, Expl, Lost);
+
     @Override
     public void onGroupComplete(int gid, TournamentMemState tournament, Set<Uid> loserUids, DbUpdater batch) {
-        loserUids.forEach(luid ->
-             bidService.setBidState(tournament.getParticipant(luid), Lost, asList(Wait, Rest), batch)
-        );
+        loserUids.stream()
+                .map(tournament::getParticipant)
+                .filter(bid -> !terminalStates.contains(bid.getBidState()))
+                .forEach(bid -> bidService.setBidState(bid, Lost, asList(Wait, Rest), batch));
     }
 }

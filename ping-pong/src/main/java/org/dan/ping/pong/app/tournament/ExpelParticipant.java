3a2b28ae8e5d8bc77dc461b39af53031e8740b8b
Daneel Yaitskov <dyaitskov@gmail.com>
2018-01-31 16:36:23 +0100

    master uid and expel validation with beans

--- ping-pong/src/main/java/org/dan/ping/pong/app/tournament/ExpelParticipant.java
+++ ping-pong/src/main/java/org/dan/ping/pong/app/tournament/ExpelParticipant.java
@@ -1,12 +1,19 @@
 package org.dan.ping.pong.app.tournament;
 
+import static org.dan.ping.pong.app.bid.BidState.Expl;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.dan.ping.pong.app.bid.BidState;
 import org.dan.ping.pong.app.bid.Uid;
+import org.dan.ping.pong.sys.validation.TidBodyRequired;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -15,6 +22,18 @@ import org.dan.ping.pong.app.bid.Uid;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExpelParticipant {
+    @Valid
+    @TidBodyRequired
     private Tid tid;
+
+    @Valid
+    @NotNull
     private Uid uid;
+
+    @NotNull
+    private BidState targetBidState = Expl;
+
+    public static class ExpelParticipantBuilder {
+        BidState targetBidState = Expl;
+    }
 }

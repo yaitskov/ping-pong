6df62f7b69d2fe8a700249c93bf7e72b0ecfcad2
Daneel Yaitskov <dyaitskov@gmail.com>
2018-02-05 16:57:03 +0100

    console ConsoleLayered

--- ping-pong-lib/src/main/java/org/dan/ping/pong/app/castinglots/rank/GroupSplitPolicy.java
+++ ping-pong-lib/src/main/java/org/dan/ping/pong/app/castinglots/rank/GroupSplitPolicy.java
@@ -1,6 +1,20 @@
 package org.dan.ping.pong.app.castinglots.rank;
 
 public enum GroupSplitPolicy {
+    /**
+     * group A => (p1, p4), group B => (p2, p3)
+     */
     BalancedMix,
-    BestToBest
+    /**
+     * group A => (p1, p2) group B => (p3, p4)
+     */
+    BestToBest,
+    /**
+     *  group A => (master group A 2, master group B 2),
+     *  group B => (master group A 3, ,master group B 3)
+     *
+     *  though actually console tournament in that case does not have groups
+     *  Groups are represented with branches of root match (JOIN type)
+     */
+    ConsoleLayered
 }

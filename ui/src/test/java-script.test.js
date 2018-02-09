3ba1ea633a08d2d499a894cc7203b8c04854567e
Daneel Yaitskov <dyaitskov@gmail.com>
2018-02-09 11:50:04 +0100

    console cc

--- ui/src/test/java-script.test.js
+++ ui/src/test/java-script.test.js
@@ -13,24 +13,24 @@ describe('java script', () => {
                  expect(new Next().f()).toBe('BA');
              });
          });
-         describe('constructor', () => {
-              it('override parent', () => {
-                  class Base {
-                      constructor() {
-                          this.base = 1;
-                      }
-                  }
+        describe('constructor', () => {
+            it('override parent', () => {
+                class Base {
+                    constructor() {
+                        this.base = 1;
+                    }
+                }
 
-                  class Next extends Base {
-                      constructor() {
-                          super(); // is required to access this
-                          this.next = 1;
-                      }
-                  }
+                class Next extends Base {
+                    constructor() {
+                        super(); // is required to access this
+                        this.next = 1;
+                    }
+                }
 
-                  expect(new Next().next).toBe(1);
-                  expect(new Next().base).toBe(1);
-              });
-         })
+                expect(new Next().next).toBe(1);
+                expect(new Next().base).toBe(1);
+            });
+        });
     });
 });

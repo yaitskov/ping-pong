505568875e51d30493cd0131f276726b18773e1e
Daneel Yaitskov <dyaitskov@gmail.com>
2018-02-09 11:48:42 +0100

    console check ES6 call super method

--- ui/src/test/java-script.test.js
+++ ui/src/test/java-script.test.js
@@ -1,5 +1,18 @@
 describe('java script', () => {
     describe('classes', () => {
+         describe('method', () => {
+             it('call overridden', () => {
+                 class Base {
+                     f() { return 'A'; }
+                 }
+
+                 class Next extends Base {
+                     f() { return 'B' + super.f(); }
+                 }
+
+                 expect(new Next().f()).toBe('BA');
+             });
+         });
          describe('constructor', () => {
               it('override parent', () => {
                   class Base {

e69b5c934d0482ae99f26981153bb6c5a4c4dfaf
Daneel Yaitskov <dyaitskov@gmail.com>
2018-02-10 20:13:18 +0100

    console new tournament ui checks

--- ui/src/tournament/parameters/param.form.component.test.js
+++ ui/src/tournament/parameters/param.form.component.test.js
@@ -13,9 +13,32 @@ describe('tournament-parameters-form', () => {
     describe('cancel', () => {
         ij('emit event', ($rootScope) => {
             const spy = spyOn($rootScope, '$broadcast');
-
+            ctx.ctrl.rules = {};
             ctx.element.find('#discard-rules-changes').click();
             expect(spy).toHaveBeenCalledWith('event.tournament.rules.cancel', ctx.ctrl.rules);
+        });
+    });
+
+    describe('back', () => {
+        ij('emit event', ($rootScope) => {
+            const spy = spyOn($rootScope, '$broadcast');
+            ctx.ctrl.rules = {};
+            ctx.element.find('#back-to-tournament-properties').click();
+            expect(spy).toHaveBeenCalledWith('event.tournament.rules.back', ctx.ctrl.rules);
+        });
+    });
+
+    describe('new tournament', () => {
+        it('update button is not visible', () => {
+            expect(ctx.element.find('#update-tournament-rules').hasClass('ng-hide')).toBeTrue();
+        });
+
+        it('create button is visible', () => {
+            expect(ctx.element.find('#create-tournament').hasClass('ng-hide')).toBeFalse();
+        });
+
+        it('back button is visible', () => {
+            expect(ctx.element.find('#back-to-tournament-properties').hasClass('ng-hide')).toBeFalse();
         });
     });
 });

111053efea43ac0cf444683bd090ac4926d3e3ac
Daneel Yaitskov <dyaitskov@gmail.com>
2018-02-15 14:41:58 +0100

    console backedUpValue for seeding panel

--- ui/src/tournament/parameters/seeding/SeedingTournamentParamsCtrl.js
+++ ui/src/tournament/parameters/seeding/SeedingTournamentParamsCtrl.js
@@ -1,4 +1,5 @@
 import BaseTrParamsCtrl from 'tournament/parameters/BaseTrParamsCtrl.js';
+import backedUpValue from 'core/backedUpValue.js';
 
 function defaultProvidedRankOptions() {
     return {maxValue: 10000, minValue: 0, label: 'rating'};
@@ -6,30 +7,22 @@ function defaultProvidedRankOptions() {
 
 export default class SeedingTournamentParamsCtrl extends BaseTrParamsCtrl {
     watchForPolicy() {
-        this.$scope.$watch('$ctrl.rules.casting.policy', (newValue, oldValue) => {
-            console.log(`rules.casting.policy: new value ${newValue} old value ${oldValue}`);
-            if (!newValue || !oldValue) {
-                return;
-            }
-            if (newValue == 'ProvidedRating') {
-                if (this.rules.casting.providedRankOptions) {
-                    return;
-                }
-                this.rules.casting.providedRankOptions = (
-                   this.providedRankOptionsBackup
-                      ? this.providedRankOptionsBackup
-                      : defaultProvidedRankOptions());
-            } else {
-                this.providedRankOptionsBackup = this.rules.casting.providedRankOptions;
-                delete this.rules.casting.providedRankOptions;
-            }
+        this.$scope.$watch('$ctrl.rules.casting.policy', (rankPolicy) => {
+            console.log(`rules.casting.policy: ${rankPolicy}`);
+            this.rules.casting.providedRankOptions = this.rankOptionsBackup.map(rankPolicy);
         });
+    }
+
+    onTournamentSet() {
+        super.onTournamentSet(...arguments);
+        this.watchForPolicy();
     }
 
     constructor() {
         super(...arguments);
         this.rank = {min: 0, max: 1000000};
-        this.providedRankOptionsBackup = null;
-        this.watchForPolicy();
+        this.rankOptionsBackup = backedUpValue(defaultProvidedRankOptions,
+                                               () => this.rules.casting.providedRankOptions,
+                                               'ProvidedRating');
     }
 }

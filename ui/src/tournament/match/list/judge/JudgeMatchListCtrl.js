1393f7185d176bd1a478b2115f024db8d4798330
Daneel Yaitskov <dyaitskov@gmail.com>
2018-03-18 16:21:04 +0100

    games-less JudgeMatchListCtrl.js

---
+++ ui/src/tournament/match/list/judge/JudgeMatchListCtrl.js
@@ -0,0 +1,99 @@
+import AngularBean from 'core/angular/AngularBean.js';
+
+export default class JudgeMatchListCtrl extends AngularBean {
+    static get $inject() {
+        return ['Match', 'Tournament', 'Participant', 'mainMenu', '$location',
+                'pageCtx', 'requestStatus', '$routeParams', 'cutil', 'binder', '$scope'];
+    }
+
+    completeMatch(match) {
+        if (this.bid) {
+            match.participants = [
+                match.enemy,
+                {uid: this.bid,
+                 name: this.cutil.findValBy(this.bids, {uid: +this.bid}).name}];
+        }
+        this.pageCtx.put('last-scoring-match', match);
+        this.$location.path('/judge/score/set/' + this.tournamentId + '/' + match.mid);
+    }
+
+    bidChange() {
+        this.matches = null;
+        this.pageCtx.put('last-bid', this.bid);
+        this.Match.bidMatchesNeedToPlay(
+            {tournamentId: this.$routeParams.tournamentId,
+             bid: this.bid
+            },
+            (matches) => {
+                this.requestStatus.complete();
+                this.matches = matches.matches;
+                this.progress = matches.progress;
+            },
+            this.requestStatus.failed);
+    }
+
+    loadRunningMatches() {
+        this.showTables = true;
+        this.orderField = 'table.label';
+        this.Match.myMatchesNeedToJudge(
+            {tournamentId: this.$routeParams.tournamentId},
+            (matches) => {
+                this.requestStatus.complete();
+                this.matches = matches.matches || [];
+                this.progress = matches.progress;
+                this.tournamentNotOpen = !matches.length;
+            },
+            this.requestStatus.failed);
+    }
+
+    loadParticipantsWithIncompletMatches() {
+        this.orderField = 'enemy.name';
+        this.showTables = false;
+        this.Participant.findByState(
+            {tid: this.$routeParams.tournamentId,
+             states: ['Wait', 'Play']
+            },
+            (bids) => {
+                this.requestStatus.complete();
+                this.bids = bids || [];
+                if (bids.length) {
+                    this.bid = "" + this.cutil.findValByO(
+                        bids, {uid: +this.pageCtx.get('last-bid')}, bids[0]).uid;
+                    this.bidChange();
+                }
+            },
+            this.requestStatus.failed);
+    }
+
+    onFormReady() {
+        this.requestStatus.startLoading();
+        this.Tournament.parameters(
+            {tournamentId: this.$routeParams.tournamentId},
+            (rules) => {
+                if (rules.place && rules.place.arenaDistribution == 'GLOBAL') {
+                    this.loadRunningMatches();
+                } else {
+                    this.loadParticipantsWithIncompletMatches();
+                }
+            },
+            this.requestStatus.failed);
+    }
+
+    bind() {
+        this.binder(this.$scope, {
+            'event.main.menu.ready': (e) => this.mainMenu.setTitle('Match Judgement'),
+            'event.request.status.ready': (event) => this.onFormReady()
+        });
+    }
+
+    constructor() {
+        super(...arguments);
+
+        this.matches = null;
+        this.tournamentId = this.$routeParams.tournamentId;
+        this.bid = null;
+        this.bids = null;
+
+        this.bind();
+    }
+}
--- ui/src/tournament/match/list/judge/judge-match-list-to-judge.component.js
+++ ui/src/tournament/match/list/judge/judge-match-list-to-judge.component.js
@@ -1,87 +1,10 @@
 import angular from 'angular';
 import './judge-match-list.scss';
+import JudgeMatchListCtrl from './JudgeMatchListCtrl.js';
 import template from './judge-match-list-to-judge.template.html';
 
 angular.module('tournament').
     component('judgeMatchListToJudge', {
         templateUrl: template,
-        controller: ['Match', 'Tournament', 'Participant', 'mainMenu', '$location',
-                     'pageCtx', 'requestStatus', '$routeParams', 'cutil', 'binder', '$scope',
-                     function (Match, Tournament, Participant, mainMenu, $location,
-                               pageCtx, requestStatus, $routeParams, cutil, binder, $scope) {
-                         var self = this;
-                         self.matches = null;
-                         self.tournamentId = $routeParams.tournamentId;
-                         self.tournamentState = null;
-                         self.bid = null;
-                         self.bids = null;
-                         self.completeMatch = function (match) {
-                             if (self.bid) {
-                                 match.participants = [
-                                     match.enemy,
-                                     {uid: self.bid,
-                                      name: cutil.findValBy(self.bids, {uid: +self.bid}).name}];
-                             }
-                             pageCtx.put('last-scoring-match', match);
-                             $location.path('/judge/score/set/' + self.tournamentId + '/' + match.mid);
-                         };
-                         self.bidChange = () => {
-                             self.matches = null;
-                             pageCtx.put('last-bid', self.bid);
-                             Match.bidMatchesNeedToPlay(
-                                 {tournamentId: $routeParams.tournamentId,
-                                  bid: self.bid
-                                 },
-                                 function (matches) {
-                                     requestStatus.complete();
-                                     self.matches = matches.matches;
-                                     self.progress = matches.progress;
-                                 },
-                                 requestStatus.failed);
-                         };
-
-                         binder($scope, {
-                             'event.main.menu.ready': (e) => mainMenu.setTitle('Match Judgement'),
-                             'event.request.status.ready': (event) => {
-                                 requestStatus.startLoading();
-                                 Tournament.parameters(
-                                     {tournamentId: $routeParams.tournamentId},
-                                     (rules) => {
-                                         if (rules.place && rules.place.arenaDistribution == 'GLOBAL') {
-                                             self.showTables = true;
-                                             self.orderField = 'table.label';
-                                             Match.myMatchesNeedToJudge(
-                                                 {tournamentId: $routeParams.tournamentId},
-                                                 (matches) => {
-                                                     requestStatus.complete();
-                                                     self.matches = matches.matches || [];
-                                                     self.progress = matches.progress;
-                                                     self.tournamentNotOpen = !matches.length;
-                                                 },
-                                                 requestStatus.failed);
-                                         } else {
-                                             self.orderField = 'enemy.name';
-                                             self.showTables = false;
-                                             Participant.findByState(
-                                                 {tid: $routeParams.tournamentId,
-                                                  states: ['Wait', 'Play']
-                                                 },
-                                                 (bids) => {
-                                                     requestStatus.complete();
-                                                     self.bids = bids || [];
-                                                     if (bids.length) {
-                                                         self.bid = "" + cutil.findValByO(
-                                                             bids, {uid: +pageCtx.get('last-bid')}, bids[0]).uid;
-                                                         self.bidChange();
-                                                     }
-                                                 },
-                                                 requestStatus.failed);
-                                         }
-                                     },
-                                     requestStatus.failed);
-                             }
-                         });
-
-                     }
-                    ]
+        controller: JudgeMatchListCtrl
     });

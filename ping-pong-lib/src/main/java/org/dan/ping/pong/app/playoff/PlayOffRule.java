c6048d28a860b07f9eedc1eb8ec033c675b06eb0
Daneel Yaitskov <dyaitskov@gmail.com>
2018-04-08 20:15:59 +0200

    master cc

--- ping-pong-lib/src/main/java/org/dan/ping/pong/app/playoff/PlayOffRule.java
+++ ping-pong-lib/src/main/java/org/dan/ping/pong/app/playoff/PlayOffRule.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.Wither;
 import org.dan.ping.pong.app.sport.MatchRules;
 
 import java.util.Optional;

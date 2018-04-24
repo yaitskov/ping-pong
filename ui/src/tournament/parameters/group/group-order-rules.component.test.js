1f9edf2d1f34e048b582ff09f1762c9e0a473ff5
Daneel Yaitskov <dyaitskov@gmail.com>
2018-04-24 20:31:15 +0200

    extra-group-matches group order rules default menu visibility

--- ui/src/tournament/parameters/group/group-order-rules.component.test.js
+++ ui/src/tournament/parameters/group/group-order-rules.component.test.js
@@ -5,11 +5,21 @@ import { newTournament } from 'test/defaultTournaments.js';
 describe('group-order-rules', () => {
     const ctx = setupAngularJs('group-order-rules');
 
+    const setRules = () => ctx.send(GroupOrderRulesCtrl.TopicTournamentRulesAvailable,
+                                    newTournament('PingPong').rules);
     it('translate group order rule label', () => {
-        ctx.send(GroupOrderRulesCtrl.TopicTournamentRulesAvailable,
-                 newTournament('PingPong').rules);
-
+        setRules();
         expect(ctx.find('a.group-rule-label:first').text()).toBe('Score Sum');
+    });
+
+    it('first rule has visible menu by default', () => {
+        setRules();
+        ctx.visible('.group-rule:first .group-rule-menu');
+    });
+
+    it('second rule has hidden menu by default', () => {
+        setRules();
+        ctx.hidden('.group-rule:eq(1) .group-rule-menu');
     });
 });
 

4817f1869ab6cc469ed99d9fdf481893a8255983
Daneel Yaitskov <dyaitskov@gmail.com>
2018-04-24 20:47:52 +0200

    extra-group-matches fix pl group order rule label translation

--- ui/src/translate/pl/pl.group.js
+++ ui/src/translate/pl/pl.group.js
@@ -5,17 +5,17 @@ module.exports = {
     'put-into-new-group': 'Do nowej grupy',
 
     'Select Group Order Rule': 'Wybierz regułę',
-    'group-rule-label-BB': 'Bilans Piłek',
-    'group-rule-label-WM': 'Wygranę Meczi',
-    'group-rule-label-f2f': 'Twarzą w Twarz',
-    'group-rule-label-LB': 'Straconę Piłki',
-    'group-rule-label-LS': 'Straconę Sety',
-    'group-rule-label-rnd': 'Losowanie',
-    'group-rule-label-SB': 'Bilans Setów',
-    'group-rule-label-WB': 'Wygranę Piłki',
-    'group-rule-label-WS': 'Wygranę Sety',
-    'group-rule-label-Punkts': 'Suma Punktów',
-    'group-rule-label-DM': 'Dodatkowe meczi',
+    'group-order-rule-label-BB': 'Bilans Piłek',
+    'group-order-rule-label-WM': 'Wygranę Meczi',
+    'group-order-rule-label-f2f': 'Twarzą w Twarz',
+    'group-order-rule-label-LB': 'Straconę Piłki',
+    'group-order-rule-label-LS': 'Straconę Sety',
+    'group-order-rule-label-rnd': 'Losowanie',
+    'group-order-rule-label-SB': 'Bilans Setów',
+    'group-order-rule-label-WB': 'Wygranę Piłki',
+    'group-order-rule-label-WS': 'Wygranę Sety',
+    'group-order-rule-label-Punkts': 'Suma Punktów',
+    'group-order-rule-label-DM': 'Dodatkowe meczi',
 
     'group-rule-parameters-title': 'Ustawenia dla: '
 };

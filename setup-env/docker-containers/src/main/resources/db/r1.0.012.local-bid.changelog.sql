0a7d0f4aeda020bcf84b06b80cae461776fcc667
Daneel Yaitskov <dyaitskov@gmail.com>
2018-06-22 13:10:28 +0200

    bid-independent-of-uid add bid to schema

--- setup-env/docker-containers/src/main/resources/db/r1.0.012.local-bid.changelog.sql
+++ setup-env/docker-containers/src/main/resources/db/r1.0.012.local-bid.changelog.sql
@@ -1,6 +1,8 @@
 --liquibase formatted sql
 --changeset diaitskov:r1.0.012
 
+ALTER TABLE tournament change rules rules varchar(900) not NULL;
+
 ALTER TABLE bid add column bid smallint unsigned NULL ;
 update bid set bid = uid;
 ALTER TABLE bid change bid bid smallint unsigned NOT NULL;
@@ -11,5 +13,7 @@ ALTER TABLE matches change uid_more bid_more smallint unsigned NULL;
 ALTER TABLE matches change uid_win bid_win smallint unsigned NULL;
 
 ALTER TABLE set_score change uid bid smallint unsigned NOT NULL;
+
+ALTER TABLE match_dispute change plaintiff plaintiff smallint unsigned NOT NULL;
 
 --rollback

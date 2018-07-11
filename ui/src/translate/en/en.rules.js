7c3b7ab7eeb25e6e585eb0f55eb07f5692684cfc
Daneel Yaitskov <dyaitskov@gmail.com>
2018-07-11 16:21:26 +0200

    bid-independent-of-uid tie break toggler in ui

--- ui/src/translate/en/en.rules.js
+++ ui/src/translate/en/en.rules.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'Tournament Rules': 'Tournament Rules'
+    'Tournament Rules': 'Tournament Rules',
+    'Play super tie break': 'Play super tie break'
 };
--- ui/src/translate/pl/pl.rules.js
+++ ui/src/translate/pl/pl.rules.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'Tournament Rules': 'Regulamin Turnieju'
+    'Tournament Rules': 'Regulamin Turnieju',
+    'Play super tie break': 'Zagrać super tie break'
 };
